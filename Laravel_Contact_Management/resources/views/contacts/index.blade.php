ef07253a6068452213b529c9f77394d3065e79d0
Nikoleta <141028635+Nikoleta79@users.noreply.github.com>
2025-02-21 19:43:58 +0100

    Add files via upload

---
+++ Laravel_Contact_Management/resources/views/contacts/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Contact List</h2>
+    <a href="{{ route('contacts.create') }}" class="btn btn-primary">Add Contact</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($contacts as $contact)
+            <tr>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->email }}</td>
+                <td>{{ $contact->phone }}</td>
+                <td>
+                    <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-warning">Edit</a>
+                    <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
---
+++ Laravel_Contact_Management/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('contacts', ContactController::class);
